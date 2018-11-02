b564d2a15e4a06f482d64fa8f1c3a0c3190b3933
Fuggly <fuggly@mac.com>
2018-11-02 12:31:14 +0100

    Crash when using Activities

--- ActivityAndExpenseEntryAPI/Models/Attachment.h
+++ ActivityAndExpenseEntryAPI/Models/Attachment.h
@@ -22,7 +22,7 @@
 /**
 * TODO: Write general description for this field
 */
-@property NSObject* identifier;
+@property NSString* identifier;
 
 /**
 * TODO: Write general description for this field
